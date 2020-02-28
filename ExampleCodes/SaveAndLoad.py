b0ad1d396a7bfb644477732a3e5c0a3f02310724
Yuan <804547696@qq.com>
2020-02-28 13:18:17 +0800

    Modified(Add explanation)

--- ExampleCodes/SaveAndLoad.py
+++ ExampleCodes/SaveAndLoad.py
@@ -5,3 +5,10 @@ torch.save(net.state_dict(), PATH)
 #load model
 net = Net()
 net.load_state_dict(torch.load(PATH))
+
+'''
+When you call torch.load() on a file which contains GPU tensors, 
+those tensors will be loaded to GPU by default. You can call 
+torch.load(.., map_location='cpu') and then load_state_dict() 
+to avoid GPU RAM surge when loading a model checkpoint.
+'''
