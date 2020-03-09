6b485cbbe06338995c2d83eab03dc34cdd9d8aa6
Yuan <804547696@qq.com>
2020-03-09 13:24:40 +0800

    Modified TrainModel

--- LearnPytorch/ExampleCodes/TrainModel.py
+++ LearnPytorch/ExampleCodes/TrainModel.py
@@ -1,3 +1,8 @@
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
